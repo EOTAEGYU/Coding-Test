72220411b7f99f385cdd811f1dad92c204fb0274
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2025-10-15 22:32:13 +0900

    [level 2] Title: 모음 사전, Time: 7.16 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    static List<String> list;
+    static String[] words = {"A", "E", "I", "O", "U"};
+    
+    public int solution(String word) {
+        list = new ArrayList<>();
+        int answer = 0;
+        int len = 0;
+        dfs("", len);
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).equals(word)){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+    
+    static void dfs(String str, int len){
+        list.add(str);
+        if(len == 5) return;
+        for(int i = 0; i < 5; i++){
+            dfs(str + words[i], len + 1);
+        }
+    }
+}
